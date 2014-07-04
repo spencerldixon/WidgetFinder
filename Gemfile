4fb299e59b01cc146c8294b4319a8448cecf2833
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-07-04 21:12:53 +0100

    Search and map for single location working, now to adjust to all

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 gem 'faker'
 gem 'geocoder'
+gem 'gmaps4rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
 # Use postgresql as the database for Active Record
--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,8 +2,17 @@ class WidgetsController < ApplicationController
 	def index
 		if params[:search].present?
 			@widgets = Widget.near(params[:search])
+					@hash = Gmaps4rails.build_markers(@widgets) do |widget, marker|
+			marker.lat widget.latitude
+			marker.lng widget.longitude
+		end
 		else
 			@widgets = Widget.all
+			@hash = Gmaps4rails.build_markers(@widgets) do |widget, marker|
+			marker.lat widget.latitude
+			marker.lng widget.longitude
 		end
+		end
+
 	end
 end
